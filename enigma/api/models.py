80ed3c75572f590dcdfb07f5a9ad212fdadfe07e
hnhaefliger <henry_haefliger@protonmail.com>
2021-03-26 21:58:10 +0100

    started adding data classes

--- enigma/api/models.py
+++ enigma/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from .utils import createUUID
+
+class DataColumn(models.Model):
+    '''
+    Class to represent a database column.
+    '''
+    public_id = models.CharField(max_length=32, default=createUUID, editable=False, unique=True)
+    name = models.CharField(max_length=128, )
+
+class DataRow(models.Model):
+    '''
+    Class to represent a database row (mostly for relationships between entries).
+    '''
+    public_id = models.CharField(max_length=32, default=createUUID, editable=False, unique=True)
+
+class DataEntry(models.Model):
+    '''
+    Class for a database entry.
+    '''
+    public_id = models.CharField(max_length=32, default=createUUID, editable=False, unique=True)
