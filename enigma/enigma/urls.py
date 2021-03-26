e28f5ab007ba2ab2dd415c354cf55ac80ab2a108
hnhaefliger <henry_haefliger@protonmail.com>
2021-03-26 21:41:29 +0100

    set up frontend template

--- enigma/enigma/urls.py
+++ enigma/enigma/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ enigma/frontend/static/frontend/main.js
@@ -0,0 +1,1 @@
+console.log('Some other code will go here');
