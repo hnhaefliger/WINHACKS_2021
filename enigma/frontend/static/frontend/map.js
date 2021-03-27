f307d5228a9b91e78315fa8b149969f49ea18207
hnhaefliger <henry_haefliger@protonmail.com>
2021-03-27 13:21:17 +0100

    refresh map when new location is added

--- enigma/frontend/static/frontend/map.js
+++ enigma/frontend/static/frontend/map.js
@@ -1,186 +1,188 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9lYWxmIiwiYSI6ImNrbXIyNHF3YTAzcHoydnBuc2x1YjRyZ3QifQ.UbmitObJVmkFyJFSC2o5cQ'; // this is also bad - remember to revoke access
 
-var features = [];
+navigator.geolocation.getCurrentPosition(position => { 
+    var features = [];
 
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
             }
         }
+        return cookieValue;
     }
-    return cookieValue;
-}
-const csrftoken = getCookie('csrftoken');
+    const csrftoken = getCookie('csrftoken');
 
-const makeFeatures = (list) => {
-    feat = []
+    const makeFeatures = (list) => {
+        feat = []
 
-    list.forEach(element => {
-        const loc = element.location.split(',');
+        list.forEach(element => {
+            const loc = element.location.split(',');
 
-        feat.push({
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [parseFloat(loc[8]), parseFloat(loc[7])]
-            },
-            'properties': {
-                'title': element.name,
-                'description': makeDescription(element),
-            }
-        })
-    });
-
-    return feat;
-}
-
-const addFacility = () => {
-    const name = document.getElementById('facilitynameinput').value;
-    const location = document.getElementById('facilitylocationinput').value;
-
-    if (name && location) {
-        $.ajax({
-            type: 'POST',
-            url: 'api/facilities/',
-            headers: {'X-CSRFToken': csrftoken},
-            data: {
-                'name': name,
-                'location': location,
-            },
-            dataType: "json",
-            success: (res) => {
-                document.getElementById('facilitynameinput').value = '';
-                document.getElementById('facilitylocationinput').value = '';
-                fetchFacilities();
-            },
-            error: (err) => {
-                console.log(err);
-                alert('Something went wrong');
-            }
+            feat.push({
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [parseFloat(loc[8]), parseFloat(loc[7])]
+                },
+                'properties': {
+                    'title': element.name,
+                    'description': makeDescription(element),
+                }
+            })
         });
-    } else {
-        alert('Please fill in all fields');
+
+        return feat;
     }
-}
 
-const newFacilityButton = document.getElementById('createfacility')
+    const addFacility = () => {
+        const name = document.getElementById('facilitynameinput').value;
+        const location = document.getElementById('facilitylocationinput').value;
 
-if (newFacilityButton) {
-    newFacilityButton.addEventListener('click', (event) => {
-        addFacility();
-    });
-};
+        if (name && location) {
+            $.ajax({
+                type: 'POST',
+                url: 'api/facilities/',
+                headers: {'X-CSRFToken': csrftoken},
+                data: {
+                    'name': name,
+                    'location': location,
+                },
+                dataType: "json",
+                success: (res) => {
+                    document.getElementById('facilitynameinput').value = '';
+                    document.getElementById('facilitylocationinput').value = '';
+                    fetchFacilities();
+                },
+                error: (err) => {
+                    console.log(err);
+                    alert('Something went wrong');
+                }
+            });
+        } else {
+            alert('Please fill in all fields');
+        }
+    }
 
-const addInstrument = () => {
-    const facility = document.getElementById('facilityid').value;
-    const instrument = document.getElementById('instrumenttypeinput').value;
-    const trained = document.getElementById('instrumenttrainedinput').value;
-    const researchers = document.getElementById('instrumentresearchersinput').value;
-    const students = document.getElementById('instrumentstudentsinput').value;
-    const publications = document.getElementById('instrumentpublicationsinput').value;
-    const samples = document.getElementById('instrumentsamplesinput').value;
+    const newFacilityButton = document.getElementById('createfacility')
 
-    if (facility && instrument && trained && researchers && students && publications && samples) {
-        $.ajax({
-            type: 'POST',
-            url: 'api/equipment/',
-            headers: {'X-CSRFToken': csrftoken},
-            data: {
-                'facility': facility,
-                'instrument': instrument,
-                'trained': trained,
-                'researchers': researchers,
-                'students': students,
-                'publications': publications,
-                'samples': samples,
-            },
-            dataType: "json",
-            success: (res) => {
-                document.getElementById('facilityid').value = '';
-                document.getElementById('instrumenttypeinput').value = '';
-                document.getElementById('instrumenttrainedinput').value = '';
-                document.getElementById('instrumentresearchersinput').value = '';
-                document.getElementById('instrumentstudentsinput').value = '';
-                document.getElementById('instrumentpublicationsinput').value = '';
-                document.getElementById('instrumentsamplesinput').value = '';
-            },
-            error: (err) => {
-                console.log(err);
-                alert('Something went wrong');
-            }
+    if (newFacilityButton) {
+        newFacilityButton.addEventListener('click', (event) => {
+            addFacility();
         });
-    } else {
-        alert('Please fill in all fields');
+    };
+
+    const addInstrument = () => {
+        const facility = document.getElementById('facilityid').value;
+        const instrument = document.getElementById('instrumenttypeinput').value;
+        const trained = document.getElementById('instrumenttrainedinput').value;
+        const researchers = document.getElementById('instrumentresearchersinput').value;
+        const students = document.getElementById('instrumentstudentsinput').value;
+        const publications = document.getElementById('instrumentpublicationsinput').value;
+        const samples = document.getElementById('instrumentsamplesinput').value;
+
+        if (facility && instrument && trained && researchers && students && publications && samples) {
+            $.ajax({
+                type: 'POST',
+                url: 'api/equipment/',
+                headers: {'X-CSRFToken': csrftoken},
+                data: {
+                    'facility': facility,
+                    'instrument': instrument,
+                    'trained': trained,
+                    'researchers': researchers,
+                    'students': students,
+                    'publications': publications,
+                    'samples': samples,
+                },
+                dataType: "json",
+                success: (res) => {
+                    document.getElementById('facilityid').value = '';
+                    document.getElementById('instrumenttypeinput').value = '';
+                    document.getElementById('instrumenttrainedinput').value = '';
+                    document.getElementById('instrumentresearchersinput').value = '';
+                    document.getElementById('instrumentstudentsinput').value = '';
+                    document.getElementById('instrumentpublicationsinput').value = '';
+                    document.getElementById('instrumentsamplesinput').value = '';
+                },
+                error: (err) => {
+                    console.log(err);
+                    alert('Something went wrong');
+                }
+            });
+        } else {
+            alert('Please fill in all fields');
+        }
     }
-}
 
-const makeDescription = (element) => {
-    return `
-            <form class="facilityform" id="createfacilityform">
-                <h2>Add a new piece of equipment</h2>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Instrument</label>
-                  <input type="email" class="form-control" id="instrumenttypeinput" placeholder="Choose an instrument type">
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Trained</label>
-                            <input type="email" class="form-control" id="instrumenttrainedinput" placeholder="#">
+    const makeDescription = (element) => {
+        return `
+                <form class="facilityform" id="createfacilityform">
+                    <h2>Add a new piece of equipment</h2>
+                    <div class="form-group">
+                    <label for="exampleInputEmail1">Instrument</label>
+                    <input type="email" class="form-control" id="instrumenttypeinput" placeholder="Choose an instrument type">
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Trained</label>
+                                <input type="email" class="form-control" id="instrumenttrainedinput" placeholder="#">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Researchers</label>
+                                <input type="email" class="form-control" id="instrumentresearchersinput" placeholder="#">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Students</label>
+                                <input type="email" class="form-control" id="instrumentstudentsinput" placeholder="#">
+                            </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Researchers</label>
-                            <input type="email" class="form-control" id="instrumentresearchersinput" placeholder="#">
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Publications</label>
+                                <input type="email" class="form-control" id="instrumentpublicationsinput" placeholder="#">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col">
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Students</label>
-                            <input type="email" class="form-control" id="instrumentstudentsinput" placeholder="#">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Samples</label>
+                                <input type="email" class="form-control" id="instrumentsamplesinput" placeholder="#">
+                            </div>
                         </div>
+                        <input type="hidden" id="facilityid" value="${element.id}" />
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Publications</label>
-                            <input type="email" class="form-control" id="instrumentpublicationsinput" placeholder="#">
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Samples</label>
-                            <input type="email" class="form-control" id="instrumentsamplesinput" placeholder="#">
-                        </div>
-                    </div>
-                    <input type="hidden" id="facilityid" value="${element.id}" />
-                </div>
-                <button type="button" id="addinstrument" class="btn btn-primary addinstrument">Add</button>
-            </form>
-    `;
-}
+                    <button type="button" id="addinstrument" class="btn btn-primary addinstrument">Add</button>
+                </form>
+        `;
+    }
 
-const fetchFacilities = () => {
-    $.ajax({
-        url: '/api/facilities/',
-        success: (res) => {
-            features = makeFeatures(res);
-        },
-    });
-}
+    const fetchFacilities = () => {
+        $.ajax({
+            url: '/api/facilities/',
+            success: (res) => {
+                const data = makeFeatures(res);
+                map.getSource('places').setData({
+                    'type': 'FeatureCollection',
+                    'features': data,
+                });
+            },
+        });
+    }
 
-fetchFacilities();
-
-navigator.geolocation.getCurrentPosition(position => { 
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/light-v10',
@@ -198,7 +200,7 @@ navigator.geolocation.getCurrentPosition(position => {
                     'type': 'geojson',
                     'data': {
                         'type': 'FeatureCollection',
-                        'features': features,
+                        'features': [],
                     }
                 });
                 
@@ -254,6 +256,8 @@ navigator.geolocation.getCurrentPosition(position => {
                 map.on('mouseleave', 'places', function () {
                     map.getCanvas().style.cursor = '';
                 });
+
+                fetchFacilities();
             }
         );
     });
