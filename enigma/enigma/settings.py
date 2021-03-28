1b7f234e700ba514bfafa18005697ac9ff479dde
hnhaefliger <henry_haefliger@protonmail.com>
2021-03-28 12:24:55 +0200

    fix heroku

--- enigma/enigma/settings.py
+++ enigma/enigma/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_heroku
+#import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,4 +147,4 @@ REST_FRAMEWORK = {
 
 # Heroku
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
